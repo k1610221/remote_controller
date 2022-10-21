ea087ea948b7b8d523e5122c94b1f694944e9e47
KANGSAN KIM <k1610221@gmail.com>
2022-10-21 17:28:01 +0900

    ver.3.0.0

---
+++ Server/README.md
@@ -0,0 +1,9 @@
+# Server
+## Overview
+A sub-project to run the http server.
+
+## Note
+"/include/my_credential.h" has been excluded on purpose of privacy. It defines "MY_SSID", "MY_PASS", "MY_AUTH" for SSID, password, security protocol of wifi, respectively.
+
+## Version
+ver.3.0.0 Basic implementation with sample codes by esp-idf <br>
---
+++ Server/src/main.c
@@ -0,0 +1,33 @@
+// Server ver.3.0.0
+#include "nvs_flash.h"
+#include "esp_http_server.h"
+#include "my_wifi.h"
+
+esp_err_t get_handler(httpd_req_t *req) {
+    const char resp[] = "<h1>Hello, world!</h1>It's been a while.";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+httpd_uri_t uri_get = {
+    .uri    = "/",
+    .method = HTTP_GET,
+    .handler= get_handler,
+    .user_ctx= NULL
+};
+
+void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    wifi_init_sta();
+
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+    if(httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &uri_get);
+    }
+}
