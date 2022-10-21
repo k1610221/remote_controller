334b822670e3f8c9e350c11b528d1ddeac967376
KANGSAN KIM <k1610221@gmail.com>
2022-10-21 23:48:47 +0900

    Basic http server

--- main/src/main.c
+++ main/src/main.c
@@ -1,5 +1,19 @@
 #include "nvs_flash.h"
 #include "wifi.h"
+#include "esp_http_server.h"
+
+esp_err_t get_handler(httpd_req_t *req) {
+    const char resp[] = "<h1>Hello, world!</h1>";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+httpd_uri_t uri_get = {
+    .uri        = "/",
+    .method     = HTTP_GET,
+    .handler    = get_handler,
+    .user_ctx   = NULL
+};
 
 void app_main(void) {
     esp_err_t ret = nvs_flash_init();
@@ -10,4 +24,10 @@ void app_main(void) {
     ESP_ERROR_CHECK(ret);
 
     wifi_init_sta();
+
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+    if(httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &uri_get);
+    }
 }
